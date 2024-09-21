43b9ebb341f3601710faf986ccf7731e0229473a
BuddhiPrasad <bp980930@gmail.com>
2024-09-21 19:15:16 +0530

    secound build error fixed

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { ArrowRight } from "lucide-react";
 import { WelcomeSettings, EmailSettings, SettingsType } from "@/types";
 
 interface PreviewProps {
